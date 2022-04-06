84c0cc4c1b728728468bce25aae566cdf0d25244
Andrey-Korn <andreykornilovich@gmail.com>
2022-04-05 21:19:17 -0700

    working dual motor test

--- src/tilt_controller.py
+++ src/tilt_controller.py
@@ -1,44 +1,43 @@
 import sys, time
 from TMC_2209_StepperDriver import *
+from mpu6050 import ANGLE_KAL, FILTER_ANGLES, MPU6050
 import pins
 from machine import Timer
+
+# get new accel ofs calibration after power cycle
+ofs = (-1428, 247, 5644, 16, 138, 125)
 
 #------------------------------------------------------
 # tilt_controller
 # 
 # create 2 TMC2209 and 1 mpu6050 drivers
-# functions:
-# 1. set target table angle
-# 2. set max_speed
-# 3. set max acceleration 
-# 4. set allowable offset between current and target
 #------------------------------------------------------
 class tilt_controller:
     x_step = None
+    x_range = None
     y_step = None
+    y_range = None
+    sensor = None
 
     def __init__(self, ustep, log=False):
         # create TMC_2209 drivers with the correct pins
         print('init')
-        # self.x_step = TMC_2209(pins._x_step, pins._x_dir, pins._x_en, serialport=1)
         self.x_step = TMC_2209(pins._x_step, pins._x_dir, pins._x_en, serialport=2)
-        # self.x_step = TMC_2209(pins._y_step, pins._y_dir, pins._y_en, serialport=2)
         self.set_parameters(self.x_step, ustep)
-        # self.x_step.setMicrosteppingResolution(3)
 
-        time.sleep(0.1)
-
-        # self.y_step = TMC_2209(pins._y_step, pins._y_dir, pins._y_en, serialport=2)
         self.y_step = TMC_2209(pins._y_step, pins._y_dir, pins._y_en, serialport=1)
         self.set_parameters(self.y_step, ustep)
-        # self.y_step.setDirection_reg(True)
-        # self.y_step.setMicrosteppingResolution(1)
 
         # turn on motor driver logs
-        # if log:
-            # self.y_step.setLoglevel(Loglevel.info)
+        if log:
+            self.y_step.setLoglevel(Loglevel.info)
+            self.x_step.setLoglevel(Loglevel.info)
 
-        print('finish')
+        # create mpu6050
+        self.sensor = MPU6050(1, pins._sda, pins._scl, ofs, 
+                              filtered=FILTER_ANGLES,
+                              anglefilter=ANGLE_KAL)
+
 
     # set parameters for a particular motor
     def set_parameters(self, motor, ustep):
@@ -51,7 +50,6 @@ class tilt_controller:
         motor.setIScaleAnalog(True)
         motor.setInterpolation(True)
         motor.setSpreadCycle(False)
-        # motor.setSpreadCycle(True)
         motor.setMicrosteppingResolution(ustep)
         motor.setInternalRSense(False)
 
@@ -69,7 +67,8 @@ class tilt_controller:
 
     # set max table tilt angles
     def set_max_angles(self, x_range, y_range):
-        pass
+        self.x_range = x_range
+        self.y_range = y_range
 
     def set_max_speed(self, motor, s):
         motor.setMaxSpeed(s)
@@ -82,13 +81,19 @@ class tilt_controller:
         print('start')
         motor.setDirection_pin(1)
         motor.setMotorEnabled(True)
+        motor.setCurrentPosition(0)
         for i in range(5):
-            motor.runToPositionSteps(1000, MovementAbsRel.relative)
-            motor.runToPositionSteps(-500, MovementAbsRel.relative)
+            motor.runToPositionSteps(50, MovementAbsRel.relative)
+            time.sleep(0.1)
+            motor.runToPositionSteps(-50, MovementAbsRel.relative)
+            time.sleep(0.1)
         
         motor.setMotorEnabled(False)
         print('finish')
     
+    def set_tilt(self, angles):
+        self.target = angles
+
     # step motors towards target angles
     def motor_callback(self, t):
         self.x_step.makeAStep()
@@ -98,37 +103,39 @@ class tilt_controller:
 
 
 
+
 # table parameters
 ustep = 3
 x_range = (-2, 2)
 y_range = (-2, 2)
+initial_target = (0, 0)
 
 # create controller
 # tilt_controller(microstep, logLevel)
-c = tilt_controller(ustep, True)
-# c = tilt_controller(ustep)
+# c = tilt_controller(ustep, True)
+c = tilt_controller(ustep)
 
 c.set_max_angles(x_range, y_range)
 
 
-c.set_max_accel(c.y_step, 2000)
-c.set_max_accel(c.x_step, 2000)
-c.set_max_speed(c.y_step, 1000)
-c.set_max_speed(c.x_step, 1000)
+c.set_max_accel(c.y_step, 1500)
+c.set_max_accel(c.x_step, 1500)
+c.set_max_speed(c.y_step, 750)
+c.set_max_speed(c.x_step, 750)
 
 
 # uncomment for various tests
 print('x_motor_test')
 # print(c.x_step.readStepsPerRevolution())
-# c.motor_test(c.x_step)
 # c.uart_test(c.x_step)
 # c.x_step.testDirStepEn()
+c.motor_test(c.x_step)
 
 print('y_motor_test')
 # print(c.y_step.readStepsPerRevolution())
-# c.motor_test(c.y_step)
 # c.uart_test(c.y_step)
 # c.y_step.testDirStepEn()
+c.motor_test(c.y_step)
 
 
 # create timers
@@ -138,13 +145,13 @@ t2 = Timer(2)
 t3 = Timer(3)
 
 # motor callback
-t0.init(period=4, callback=c.motor_callback)
+# t0.init(period=4, callback=c.motor_callback)
 
 # accelerometer callback
 # t1.init(period=5, callback=c.)
 
 # deinit
-del c
+# del c
 
 # spin
 while(1):
