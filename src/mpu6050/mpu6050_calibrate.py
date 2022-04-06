02c5ffd756e84a38728e21a27f8dc19c186524cd
Andrey-Korn <andreykornilovich@gmail.com>
2022-04-05 20:52:58 -0700

    enabled k-filter

--- src/mpu6050/mpu6050_calibrate.py
+++ src/mpu6050/mpu6050_calibrate.py
@@ -1,8 +1,8 @@
-from mpu6050 import MPU6050
-
-# ESP32 default scl/sda pins
-scl_pin_num = 22
-sda_pin_num = 21
+from mpu6050 import ANGLE_KAL, FILTER_ANGLES, MPU6050
+import pins
 
 # record ofs output of the following line, and add it to MPU6050 config
-MPU6050(1, sda_pin_num, scl_pin_num)
+# MPU6050(1, pins._sda, pins._scl)
+MPU6050(1, pins._sda, pins._scl, 
+        filtered=FILTER_ANGLES, 
+        anglefilter=ANGLE_KAL)
--- src/mpu6050/mpu6050_test.py
+++ src/mpu6050/mpu6050_test.py
@@ -1,24 +1,24 @@
 from machine import SoftI2C, Pin, Timer
-from mpu6050 import MPU6050
+from mpu6050 import ANGLE_KAL, FILTER_ANGLES, MPU6050
+import pins
 
 # get new ofs calibration after power cycle
-ofs = (-3094, 205, 3754, 9, 111, 61)
-
-# ESP32 default scl/sda pins
-scl_pin_num = 22
-sda_pin_num = 21
+ofs=(-1428, 247, 5644, 16, 138, 125)
 
 tim0 = Timer(0)
 
 # iterrupt/callback
 def sensor_callback(t):
     if 'mpu' in globals():
-        # mpu.print_angles()
-        mpu.print_all()
+        mpu.print_angles()
+        # mpu.print_all()
         # mpu.print_data()
 
 # create accelerometer
-mpu = MPU6050(1, sda_pin_num, scl_pin_num, ofs)
+mpu = MPU6050(1, pins._sda, pins._scl, ofs, 
+              filtered=FILTER_ANGLES,
+              anglefilter=ANGLE_KAL)
+
 if not mpu.passed_self_test:
     print('MPU6050 init error!')
     exit()
